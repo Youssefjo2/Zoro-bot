87faeddf4afdbffa0a37807c0e4b3c03e96de023
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2023-12-29 09:01:18 +0200

    Update maker-blur.js

--- plugins/maker-blur.js
+++ plugins/maker-blur.js
@@ -2,9 +2,9 @@ const handler = async (m, {conn, usedprefix}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/blur', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'hornycard.png', '*[ ✔ ] 𝙴𝙵𝙴𝙲𝚃𝙾 𝙰𝙿𝙻𝙸𝙲𝙰𝙳𝙾*', m);
+  }), 'hornycard.png', '*[ ✔ ] اتفضل يحب*', m);
 };
 handler.help = ['blur', 'difuminar2'];
 handler.tags = ['maker'];
-handler.command = /^(blur|difuminar2)$/i;
+handler.command = /^(blur|بلور)$/i;
 export default handler;
